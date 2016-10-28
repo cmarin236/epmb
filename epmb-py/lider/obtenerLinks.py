3899ffb461e91dd4205837faa774df1a3f652eff
cmarin236 <cmarin236@gmail.com>
2016-10-28 18:01:56 -0300

    Se modifica obtenerLinks.py

--- epmb-py/lider/obtenerLinks.py
+++ epmb-py/lider/obtenerLinks.py
@@ -3,11 +3,11 @@ import unicodedata
 import MySQLdb
 from bs4 import BeautifulSoup
 
-DB_HOST = raw_input("Servidor   : ")
+DB_HOST = "integraredes.cl"
 DB_USER = raw_input("Usuario    : ")
 DB_PASS = raw_input("Password   : ")
-DB_NAME = raw_input("Base Datos : ")
-DB_TABLA= raw_input("Tabla      : ")
+DB_NAME = "cin16445_epmb"
+DB_TABLA= "links_jumbo"
 
 def run_query(query=''):
 	datos = [DB_HOST, DB_USER, DB_PASS, DB_NAME]
@@ -34,4 +34,4 @@ result = run_query(query)
 
 
 for registro in run_query(query):
-	print (registro[0])
+	print (registro[2])
